c5d462a7a00e262216c39c9c61f0bc3344bdd696
Xplosion257 <eric.jiyu.wang@gmail.com>
2025-11-13 16:45:26 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# Imports
+import random
+
+# Variables
+coinScore = 0
+
+# Functions
+def coinflip():
+  global coinScore
+  while True:
+    answer = input("Do you want to flip a coin? (y/n)\n ")
+    if answer == "n":
+      print(f"Your score was {coinScore}!")
+      break
+    result = random.randint(1, 2)
+    if result == 2:
+      coinScore += 1
+
+# The main loop
+coinflip()
